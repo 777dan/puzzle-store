fd4fbd0218fa0a230c1f587b16296470cee23469
777dan <danya.nikitin7@gmail.com>
2024-03-18 10:12:24 +0200

    added cart examaple

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 document.onreadystatechange = function () {
   let state = document.readyState;
   if (state == "interactive") {
-    document.getElementById("contents").style.visibility = "hidden";
+    document.getElementById("contents").style.display = "none";
+    // document.getElementById("contents").style.visibility = "hidden";
     document.getElementById("load").style.visibility = "visible";
   } else if (state == "complete") {
     setTimeout(function () {
       document.getElementById("load").style.visibility = "hidden";
-      document.getElementById("contents").style.visibility = "visible";
+      document.getElementById("contents").style.display = "block";
     }, 2000);
   }
 };
@@ -17,12 +18,16 @@ $(document).ready(function () {
     for (let i = 0; i < puzzArr.length; i++) {
       $("#puzzles-section").append(`
       <div class='col-3 my-3'>
-        <div class="card-header">
+        <div class="card-header card-img">
             <img class="col-12" src='${puzzArr[i].img}' alt=''>
         </div>
         <div class="card-body">${puzzArr[i].name}</div>
         <div class="card-footer">${puzzArr[i].price}$</div>
       </div>`);
     }
+  }).done(function () {
+    $(".card-img").click(function () {
+      console.log($(this));
+    });
   });
 });
