4f70763a82c63368244dc464b21f82a516dc5858
777dan <danya.nikitin7@gmail.com>
2024-03-18 13:11:03 +0200

    the cart was imroved

--- script.js
+++ script.js
@@ -27,18 +27,6 @@ $(document).ready(function () {
         <div class="card-footer">${puzzArr[i].price}$</div>
       </div>`);
     }
-  }).done(function () {
-    $(".card-img").click(function () {
-      let itemId = $(this).attr('data-id');
-      // console.log($(this).attr('data-id'));
-      let cartData = getCartData() || {};
-      if (cartData.hasOwnProperty(itemId)) {
-        cartData[itemId][2] += 1;
-      } else {
-        cartData[itemId] = [puzzlesArr[itemId].name, puzzlesArr[itemId].price, 1, puzzlesArr[itemId].img];
-      }
-      setCartData(cartData);
-    });
   });
   function getCartData() {
     return JSON.parse(localStorage.getItem("cart"));
@@ -47,57 +35,77 @@ $(document).ready(function () {
     localStorage.setItem("cart", JSON.stringify(cartData));
   }
   $("#open-cart").click(function () {
+    openCart();
+  });
+  $("#clear-cart").click(function () {
+    clearCart();
+    openCart();
+  });
+  $(document).on("click", "#add-to-cart", function () {
+    addToCart($(this));
+    openCart();
+  });
+  $(document).on("click", "#del-from-cart", function () {
+    delFromCart($(this));
+    openCart();
+  });
+  $(document).on("click", ".card-img", function () {
+    addToCart($(this));
+  });
+
+  function addToCart(addButt) {
+    let itemId = addButt.attr('data-id');
+    let cartData = getCartData() || {};
+    if (cartData.hasOwnProperty(itemId)) {
+      cartData[itemId][2] += 1;
+    } else {
+      cartData[itemId] = [puzzlesArr[itemId].name, puzzlesArr[itemId].price, 1, puzzlesArr[itemId].img];
+    }
+    setCartData(cartData);
+  }
+
+  function delFromCart(delButt) {
+    let cartData = getCartData() || {};
+    let itemId = delButt.attr('data-id');
+    if (cartData.hasOwnProperty(itemId)) {
+      if (cartData[itemId][2] - 1 !== 0) {
+        cartData[itemId][2] -= 1;
+      } else {
+        delete cartData[itemId];
+      }
+    }
+
+    setCartData(cartData);
+  }
+
+  function openCart() {
     let cartData = getCartData();
+    let totalAmount = 0;
     if (cartData !== null) {
+      $("#puzz-list").empty();
       for (const items in cartData) {
+        totalAmount += cartData[items][1] * cartData[items][2];
         $("#puzz-list").append(`
         <div class="row d-flex align-items-center">
           <img class="col-sm-2" src="${cartData[items][3]}" width="20%" alt="">
           <span class="col-sm-2" id="name">${cartData[items][0]}</span>
-          <span class="col-sm-2" id="price">${cartData[items][1] * cartData[items][2]}$</span>
+          <span class="col-sm-2" id="price">${(cartData[items][1] * cartData[items][2]).toFixed(2)}$</span>
           <span class="col-sm-2" id="quantity">${cartData[items][2]}</span>
           <div class="col-sm-4" id="del-add">
-            <button class="btn btn-success">&#43;</button>
-            <button class="btn btn-danger">&#8722;</button>
+            <button id="add-to-cart" class="btn btn-success" data-id="${items}">&#43;</button>
+            <button id="del-from-cart" class="btn btn-danger" data-id="${items}">&#8722;</button>
            </div>
          </div>
         `);
       }
-      // for (let i = 0; i < cartData.length; i++) {
-      //   $("#puzz-list").append(`
-      //   <div class="d-flex align-items-center gap-5">
-      //     <img src="./images/puzzles/alpen.jpg" width="20%" alt="">
-      //     <span id="name">${cartData[i].name}</span>
-      //     <span id="price">${cartData[i].price}</span>
-      //     <div id="del-add">
-      //       <button class="btn btn-success">&#43;</button>
-      //       <button class="btn btn-danger">&#8722;</button>
-      //      </div>
-      //    </div>
-      //   `);
-      // }
+      $("#total-amount").html(totalAmount.toFixed(2));
     } else {
       $("#puzz-list").append("<p>The cart is empty</p>");
     }
-  });
-});
+  }
 
-// let cardList = "";
-// cardList =
-//   '<table class="shopping_list"><tr><th>Name</th><th>Price</th><th>Quantity</th></tr>';
-// let totalCost = 0;
-// for (let items in cartData) {
-//   cardTable += "<tr>";
-//   for (let i = 0; i < cartData[items].length; i++) {
-//     if (i === 1) {
-//       cardTable += "<td>" + Number(cartData[items][i]) * cartData[items][2] + "</td>";
-//       totalCost += Number(cartData[items][i]) * cartData[items][2];
-//     } else {
-//       cardTable += "<td>" + cartData[items][i] + "</td>";
-//     }
-//   }
-//   cardTable += "</tr>";
-// }
-// cardTable += "<table>";
-// cartCont.innerHTML = cardTable;
-// cartCont.innerHTML += `Total cost: ${totalCost}$`;
+  function clearCart() {
+    localStorage.removeItem("cart");
+    cartCont.innerHTML = "The cart was emtied";
+  }
+});
